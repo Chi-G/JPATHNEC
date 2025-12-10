bb10720629592ef928d858433efb91448f8de98e
Chi-G <chijindu.nwokeohuru@gmail.com>
2025-12-10 10:20:15 +0100

    shared hosting cookie issues

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Facades\Filament;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerViteTheme('resources/css/filament.css');
+        });
+
+        // Fix for shared hosting cookie issues
+        config(['session.same_site' => 'lax']);
+    }
+}
